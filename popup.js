4a43e32ff65d8edb3a8c1b0904bff0f96b9957ef
itsmekajal10 <kajalgadade0510@gmail.com>
2023-07-09 22:16:50 +0530

    typescript
    typescript

--- popup.js
+++ popup.js
@@ -1,54 +1,36 @@
 console.log("Hello");
 alert("Hello");
-function newConfirm(test?: string) {
-
+function newConfirm(test) {
 }
 confirm("Do you really want to do this?");
 var data = prompt("Please enter your name");
-console.log('Entered name', data)
-
+console.log('Entered name', data);
 // unknown
-
-let value : unknown;
-
+var value;
 // value = true; // OK 
-
 // value = [];
 // value = Math.random;
 // value = null;
 // value = undefined;
-
 //let value1 : unknown = value;
 //let value2 : any = value;
-
 //let value3 : boolean = value;
-
 //let value4 : number = value;
-
 //let value5 : string = value;
-
 //let value6 : object = value;
-
 //let value7 : any[] = value;
-
-function loadString(): any {
+function loadString() {
     return 1234567;
 }
-
-function loadnumber(): any {
-    return 123
+function loadnumber() {
+    return 123;
 }
-
-
-let someVariable : unknown;
+var someVariable;
 someVariable = loadString();
-
 if (typeof someVariable == 'string') {
     console.log(someVariable.trim());
 }
-
 someVariable = loadnumber();
-
 if (typeof someVariable == 'number') {
     console.log(someVariable.toFixed(2));
 }
